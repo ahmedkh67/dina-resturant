7a508c8a3fe2126717857ba2e7c67988c1806e2c
ahmedkh67 <al.monster.games@gmail.com>
2025-03-19 04:39:02 +0300

    linktest

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,9 @@ export default function Home() {
         
         <GalleryButton title={Gallerybuttontitle} link={'/Gallery'} bio={"Check our photos taken from the kitchen !"} />
         <div className="flex w-full pb-8 ">
-            <MenusecsButtons />
+            <Link href={'/menu'} >
+                <MenusecsButtons />
+            </Link>
         </div>
         <Moresec />
         <div className="flex w-full justify-start flex-col items-start bg-gray-200 md:pl-25 pl-6">
